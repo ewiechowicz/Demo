3d8cfbd1e0baf7d384c3352c7910172f27243e67
Ewelina Wiechowicz <ewelina.wiechowicz@gmail.com>
2016-11-20 14:19:36 +0100

    CSS fix #2

---
+++ calculator.jsx
@@ -0,0 +1,113 @@
+const Calculator = React.createClass({
+  getInitialState() {
+    return {
+      operator: '+',
+      number: '',
+      number2: '',
+    };
+  },
+
+  onSelectChange(event) {
+    this.setState({
+      operator: event.target.value,
+    });
+  },
+  onFirstNumberChange(event) {
+    this.setState({
+      number: Number(event.target.value),
+    });
+  },
+  onSecondNumberChange(event) {
+    this.setState({
+      number2: Number(event.target.value),
+    });
+  },
+
+  // componentDidMount() {
+  //   $(this.refs.dropdown).dropdown({
+  //     action: this.onSelectChange,
+  //   });
+  // },
+
+  render() {
+    let result;
+    switch (this.state.operator) {
+      case '+':
+        result = this.state.number + this.state.number2;
+        break;
+
+      case '-':
+        result = this.state.number - this.state.number2;
+        break;
+
+      case '*':
+        result = this.state.number * this.state.number2;
+        break;
+
+      case '/':
+        result = this.state.number / this.state.number2;
+        break;
+
+      default:
+    }
+
+    // let xResult;
+    // if (result || result === 0) {
+    //   xResult = <b>= {result}</b>;
+    // }
+
+    return (
+      <div
+        className="ui two column centered grid"
+        style={{
+          marginTop: 200,
+        }}
+      >
+        <div className="column">
+          <div className="ui large action input">
+            <input
+              type="text"
+              placeholder="Add number..."
+              onChange={this.onFirstNumberChange}
+              value={this.state.number}
+            />
+            &nbsp;
+            <select
+              className="ui massive compact selection dropdown"
+              value={this.state.operator}
+              onChange={this.onSelectChange}
+            >
+              <option value="+">Dodawanie</option>
+              <option value="-">Odejmowanie</option>
+              <option value="*">Mnożenie</option>
+              <option value="/">Dzielenie</option>
+            </select>
+            &nbsp;
+            <input
+              type="text"
+              placeholder="Add number..."
+              onChange={this.onSecondNumberChange}
+              value={this.state.number2}
+            />
+            <button className="ui blue button">{result}</button>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <img
+            alt="calculator"
+            src="./calculator.png"
+            heigh="50%"
+            width="50%"
+          />
+        </div>
+      </div>
+    );
+  },
+});
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('app')
+);
